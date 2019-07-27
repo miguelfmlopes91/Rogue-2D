c9bb27d9cc32f57bdc5d6fdef5a4020657c1ed7b
Miguel Lopes <miguel.lopes@miniclip.com>
2019-07-27 22:12:35 +0100

    Add rogue intial files

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class GameManager : MonoBehaviour{
+
+    public static GameManager instance = null;
+
+    public float levelStartDelay = 2f;
+    public float turnDelay = .1f;
+    public BoardManager boardScript;
+    public int playerFoodPoints = 100;
+    [HideInInspector] public bool playerTurn = true;
+
+    private Text levelText;
+    private GameObject levelImage;
+    private int level = 1;
+    private List<Enemy> enemies;
+    private bool enemiesMoving;
+    private bool doingSetup = true;
+
+    // Start is called before the first frame update
+    void Awake(){
+
+        if(instance == null) {
+            instance = this;
+        }
+        else if(instance  != this){
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+        enemies = new List<Enemy>();
+        boardScript = GetComponent<BoardManager>();
+        InitGame();
+    }
+
+    //this is called only once, and the paramter tell it to be called only after the scene was loaded
+    //(otherwise, our Scene Load callback would be called the very first load, and we don't want that)
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
+    static public void CallbackInitialization(){
+        //register the callback to be called everytime the scene is loaded
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    //This is called each time a scene is loaded.
+    static private void OnSceneLoaded(Scene arg0, LoadSceneMode arg1){
+        instance.level++;
+        instance.InitGame();
+    }
+
+    void InitGame(){
+        doingSetup = true;
+
+        levelImage = GameObject.Find("LevelImage");
+        levelText = GameObject.Find("LevelText").GetComponent<Text>();
+        levelText.text = "Day " + level;
+        levelImage.SetActive(true);
+
+        Invoke("HideLevelImage", levelStartDelay);
+
+        enemies.Clear();
+        boardScript.SetupScene(level);
+    }
+
+    private void HideLevelImage() {
+        levelImage.SetActive(false);
+        doingSetup = false;
+
+    }
+
+    public void GameOver() {
+        levelText.text = "After " + level + " of days, you starved.";
+        levelImage.SetActive(true);
+        enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update(){
+        if (playerTurn || enemiesMoving || doingSetup){
+            return;
+        }
+
+        StartCoroutine(MoveEnemies());
+    }
+
+    public void AddEnemyToList(Enemy script) {
+        enemies.Add(script);
+    }
+
+    IEnumerator MoveEnemies(){
+        enemiesMoving = true;
+        yield return new WaitForSeconds(turnDelay);
+
+        if (enemies.Count == 0){
+            yield return new WaitForSeconds(turnDelay);
+        }
+
+        for (int i = 0; i < enemies.Count; i++){
+            enemies[i].MoveEnemy();
+            yield return new WaitForSeconds(enemies[i].moveTime);
+
+        }
+        playerTurn = true;
+        enemiesMoving = false;
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class Player : MovingObject{
+    public int wallDamage = 1;
+    public int pointsForFood = 10;
+    public int pointsForSoda = 20;
+    public float restartLevelDelay = 1f;
+    public Text foodText;
+    public AudioClip moveSound1;
+    public AudioClip moveSound2;
+    public AudioClip eatSound1; 
+    public AudioClip eatSound2; 
+    public AudioClip drinkSound1;
+    public AudioClip drinkSound2;
+    public AudioClip gameOverSound;
+
+
+    private Animator animator;
+    private int food;
+
+
+
+    // Start is called before the first frame update
+    protected override void Start(){
+        animator = GetComponent<Animator>();
+        food = GameManager.instance.playerFoodPoints;
+        foodText.text = "Food: " + food;
+        base.Start();
+    }
+
+    private void OnDisable(){
+        GameManager.instance.playerFoodPoints = food;
+    }
+
+    // Update is called once per frame
+    void Update(){
+        if (!GameManager.instance.playerTurn){
+            return;
+        }
+
+        int horizontal;
+        int vertical;
+
+        horizontal = (int)Input.GetAxisRaw("Horizontal");
+        vertical = (int)Input.GetAxisRaw("Vertical");
+
+        if (horizontal!=0){
+            vertical = 0;
+        }
+
+        if (horizontal!=0 || vertical!=0){
+            AttemptMove<Wall>(horizontal, vertical);
+        }
+    }
+
+    protected override void AttemptMove<T>(int xDir, int yDir){
+        --food;
+        foodText.text = "Food: " + food;
+
+        base.AttemptMove<T>(xDir, yDir);
+
+        RaycastHit2D hit;
+        if (Move(xDir,yDir, out hit)){
+            SoundManager.instance.RandomizeSfx(moveSound1, moveSound2);
+        }
+
+        checkIfGameOver();
+
+        GameManager.instance.playerTurn = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other){
+        if (other.tag == "Exit"){
+            Invoke("Restart", restartLevelDelay);
+            enabled = false;
+        }
+        else if(other.tag == "Food"){
+            food += pointsForFood;
+            foodText.text = "+" + pointsForFood + " Food: " + food;
+            SoundManager.instance.RandomizeSfx(eatSound1, eatSound2);
+            other.gameObject.SetActive(false);
+        }
+        else if (other.tag == "Soda")
+        {
+            food += pointsForSoda;
+            foodText.text = "+" + pointsForSoda + " Food: " + food;
+            SoundManager.instance.RandomizeSfx(drinkSound1, drinkSound2);
+            other.gameObject.SetActive(false);
+        }
+    }
+
+    protected override void OnCantMove<T>(T component){
+        Wall hitWall = component as Wall;
+        hitWall.DamageWall(wallDamage);
+        animator.SetTrigger("playerChop");
+    }
+
+    private void Restart() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex, LoadSceneMode.Single);
+    }
+
+    public void loseFood(int loss) {
+        animator.SetTrigger("playerhit");
+        food -= loss;
+        foodText.text = "-" + loss + " Food: " + food;
+
+        checkIfGameOver();
+    }
+
+    private void checkIfGameOver() {
+        if (food <= 0){
+            SoundManager.instance.RandomizeSfx(gameOverSound);
+            SoundManager.instance.musicSource.Stop();
+            GameManager.instance.GameOver();
+        }
+    }
+}
